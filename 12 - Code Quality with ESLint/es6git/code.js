1decfec8398e9d4ad57fc6f92009220154bc2450
Libby Schumacher-Knight <libbysk@gmail.com>
2026-01-13 07:51:33 +1300

    Module 12 completed

---
+++ 12 - Code Quality with ESLint/es6git/code.js
@@ -0,0 +1,13 @@
+// const x= 100;
+// console.log('this shyld be a waerning');
+
+
+// const tweets = new Promise((resolve, reject) => {
+//   setTimeout(() =>{
+//     resolve(['I like cake', 'BBQ is good too!']);
+//   }, 500);
+// });
+
+const x = 100;
+// eslint-disable-next-line no-console
+console.log(x);
---
+++ 12 - Code Quality with ESLint/readme.md
@@ -0,0 +1,3 @@
+# Module 12
+
+Add git repo but now not git, to show how to add a git hook with linting.
